8e500af1a671449ccca2a58c10a17578ff7d7272
Victor-Gabriel Nylev <117641530+vgn40@users.noreply.github.com>
2025-09-16 14:35:17 +0200

    branch push test

---
+++ rewrite/go.mod
@@ -0,0 +1,5 @@
+module devops-valgfag/rewrite
+
+go 1.24.7
+
+require github.com/gorilla/mux v1.8.1
--- rewrite/main.go
+++ rewrite/main.go
@@ -1,1 +1,32 @@
+﻿package main
 
+import (
+    "fmt"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+        fmt.Fprint(w, `<!doctype html>
+<html>
+<head><meta charset="utf-8"><title>Search</title></head>
+<body>
+  <div>
+    <input id="search-input" placeholder="Search..." />
+    <button id="search-button" onclick="alert('Søgning kobles på i næste trin')">Search</button>
+  </div>
+</body>
+</html>`)
+    }).Methods("GET")
+
+    addr := ":8080"
+    println("Server kører på http://localhost" + addr)
+    if err := http.ListenAndServe(addr, r); err != nil {
+        panic(err)
+    }
+}
